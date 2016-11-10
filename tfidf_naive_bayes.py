7c65162e5a384267c8f2233725c80b44b52cf10b
varunbezzam <varun.bezzam@gmail.com>
2016-11-09 19:10:28 -0500

    Added SVM Code. Added subreddit datasets

--- tfidf_naive_bayes.py
+++ tfidf_naive_bayes.py
@@ -1,34 +1,59 @@
+# SELECT * FROM (SELECT ROW_NUMBER() OVER(), created_utc,subreddit,title
+# FROM [fh-bigquery:reddit_posts.2016_08]
+# WHERE subreddit = 'gaming') WHERE f0_ >= 20001 AND f0_ <= 30000;
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn import cross_validation
 
+# Loads required data
 def load_data(filename):
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename, encoding = 'latin-1')
     return (df['title'],df['subreddit'])
 
-def create_tfidf(X):
-    tf = TfidfVectorizer()
-    X_tfidf = tf.fit_transform(X)
-    return (tf,X_tfidf)
+# Creates a Naive Bayes Classifier
+def create_nb_classifier(X,y):
+    text_clf = Pipeline([('vect',CountVectorizer()),
+        ('tfidf',TfidfTransformer()),
+        ('clf',MultinomialNB()),
+        ])
+    text_clf = text_clf.fit(X,y)
+    return text_clf
 
-def create_model(X,y):
-    clf = MultinomialNB().fit(X,y)
-    return clf
-
-def find_subreddit(tf,clf,X_test):
-    td = tf.transform(X_test)
-    return clf.predict(td)
+# Creates an SVM Classifier
+def create_svm_classifier(X,y):
+    svm_text_clf = Pipeline([('vect',CountVectorizer()),
+        ('tfidf',TfidfTransformer()),
+        ('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-4, n_iter=5, random_state=42)),
+        ])
+    svm_text_clf = svm_text_clf.fit(X,y)
+    return svm_text_clf
 
 if __name__ == "__main__":
     X_list = []
     y_list = []
-    for i in ['gatech-aug2016.csv','joker-aug2016.csv']:
+    data_file_list = [
+    'Data/Subreddits/books.csv',
+    'Data/Subreddits/food.csv',
+    'Data/Subreddits/gaming.csv',
+    'Data/Subreddits/sports.csv',
+    'Data/Subreddits/worldnews.csv',
+    'Data/Subreddits/art.csv',
+    'Data/Subreddits/music.csv',
+    'Data/Subreddits/history.csv',
+    'Data/Subreddits/askscience.csv',
+    ]
+    for i in data_file_list:
+        print(i)
         (text,sub) = load_data(i)
         X_list.append(text)
         y_list.append(sub)
     X = pd.concat(X_list)
     y = pd.concat(y_list)
-    (tf,X) = create_tfidf(X)
-    clf = create_model(X,y)
-    results = find_subreddit(tf,clf,['Suicide Squad','UGA is an absolute joke','Tech is amazing!','Heath Ledger','The Killing Joke','College is expensive'])
-    print(results)
+    text_clf = create_nb_classifier(X,y)
+    scores = cross_validation.cross_val_score(text_clf,X,y,cv = 5)
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
